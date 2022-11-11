8229889b1671f85ce6c79acacb69abd9733cc92c
L00GLE <37436137+L00GLE@users.noreply.github.com>
2022-11-11 15:26:05 -0800

    Added Greeting to Nassef

---
+++ pyramidapp/src/main/java/pyramiddemo/App.java
@@ -0,0 +1,13 @@
+package pyramiddemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Nassef!" );
+    }
+}
