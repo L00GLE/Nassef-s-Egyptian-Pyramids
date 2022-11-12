3a5573b7dd75b7b44f080a330fd9f89507f73a4e
L00GLE <37436137+L00GLE@users.noreply.github.com>
2022-11-12 13:44:28 -0800

    Added More Menu Display Commands

---
+++ pyramidapp/src/main/java/pyramiddemo/Pharaoh.java
@@ -0,0 +1,11 @@
+package pyramiddemo;
+
+public class Pharaoh {
+
+    public Pharaoh(Integer id, String name, Integer begin, Integer end, Integer contribution, String hieroglyphic) {
+    }
+
+    public void print() {
+    }
+
+}
